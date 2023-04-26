0259bc0e4e8a22369c35cba92fe34efd5a03f7bc
AK-8778232465 <113067573+AK-8778232465@users.noreply.github.com>
2023-04-26 11:36:55 +0530

    update

---
+++ storage/framework/views/c16d4f4180201dcec89a0f4060921997.php
@@ -0,0 +1,5 @@
+<a href="/">
+    
+    <img src="<?php echo e(asset('logo/DD.png')); ?>" alt="Logo">
+</a>
+<?php /**PATH D:\Auth\resources\views/components/authentication-card-logo.blade.php ENDPATH**/ ?>
