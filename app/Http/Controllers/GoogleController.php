89beabb417359a8ca6e538f7baae5d7ae6aa57f3
AK-8778232465 <113067573+AK-8778232465@users.noreply.github.com>
2023-04-26 15:48:15 +0530

    fix

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 use Exception;
 use App\Models\User;
+use App\Models\Team;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Jetstream\Jetstream;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use DB;
 
 class GoogleController extends Controller
 {
@@ -40,12 +44,19 @@ class GoogleController extends Controller
                 return redirect()->intended('dashboard');
 
             }else{
+
                 $newUser = User::create([
                     'name' => $user->name,
                     'email' => $user->email,
                     'google_id'=> $user->id,
-                    'password' => encrypt('123456dummy')
+                    'password' => encrypt('User@123$')
                 ]);
+
+                $user->ownedTeams()->save(Team::forceCreate([
+                    'user_id' => $user->id,
+                    'name' => explode(' ', $user->name, 2)[0]."'s Team",
+                    'personal_team' => true,
+                ]));
 
                 Auth::login($newUser);
 
