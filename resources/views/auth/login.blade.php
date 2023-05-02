d453ac31c77349c89abb42c84436a4c3f4149cf0
AK-8778232465 <113067573+AK-8778232465@users.noreply.github.com>
2023-05-02 13:19:41 +0530

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
         @elseif (session('duplicate'))
         <div class="font-medium text-red-600">{{ __('Whoops! Something went wrong.') }}</div>
             <ul class="mt-3 list-disc list-inside text-sm text-red-600">
-                    <li>{{ session('status') }}</li>
+                    <li>{{ session('duplicate') }}</li>
             </ul>
         @endif
 
