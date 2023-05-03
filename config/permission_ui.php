4a55cb79fe4e06c6124abd089e8049c5cf2e1f44
AK-8778232465 <113067573+AK-8778232465@users.noreply.github.com>
2023-05-03 16:58:53 +0530

    Update permission_ui.php

--- config/permission_ui.php
+++ config/permission_ui.php
@@ -1,16 +1,9 @@
 <?php
 
-namespace Illuminate\Support\Facades;
-use Illuminate\Support\Facades\Cache;
-
-
-// Cache::put('uid', uniqid());
-
-
 
 return [
     'middleware'        => ['web', 'auth'],
-    'url_prefix'        => 'permissions1',
+    'url_prefix'        => 'permissions',
     'route_name_prefix' => 'permission_ui.',
 ];
 
