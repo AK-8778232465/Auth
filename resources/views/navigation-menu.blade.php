59330c506758cc446e96f8723cab5872974c3519
AK-8778232465 <113067573+AK-8778232465@users.noreply.github.com>
2023-05-03 18:14:40 +0530

    aa

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -60,7 +60,7 @@
                                     <!-- Team Switcher -->
                                     @if (Auth::user()->allTeams()->count() >= 1)
                                         <div class="border-t border-gray-200"></div>
-                                        @if (Auth::user()->allTeams()->count() > 1)
+                                        @if (Auth::user()->allTeams()->count() >= 1)
                                             <div class="block px-4 py-2 text-xs text-gray-400">
                                                 {{ __('Switch Teams') }}
                                             </div>
