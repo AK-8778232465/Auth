ee1a9cd5daa84cfd9716d7c7d55991a47c901a27
AK-8778232465 <113067573+AK-8778232465@users.noreply.github.com>
2023-05-04 16:29:37 +0530

    Update MessagesController.php

--- vendor/munafio/chatify/src/Http/Controllers/MessagesController.php
+++ vendor/munafio/chatify/src/Http/Controllers/MessagesController.php
@@ -224,8 +224,16 @@ class MessagesController extends Controller
         $users = Message::join('users',  function ($join) {
             $join->on('ch_messages.from_id', '=', 'users.id')
                 ->orOn('ch_messages.to_id', '=', 'users.id');
-        });
-
+        })
+        ->where(function ($q) {
+            $q->where('ch_messages.from_id', Auth::user()->id)
+            ->orWhere('ch_messages.to_id', Auth::user()->id);
+        })
+        ->where('users.id','!=',Auth::user()->id)
+        ->select('users.*',DB::raw('MAX(ch_messages.created_at) max_created_at'))
+        ->orderBy('max_created_at', 'desc')
+        ->groupBy('users.id')
+        ->paginate($request->per_page ?? $this->perPage);
 
         $usersList = $users->items();
 
